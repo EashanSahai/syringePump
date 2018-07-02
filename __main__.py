92e28a90a096c5d05b87b1fde05df55942ec76f6
Eashan Sahai <eashansahai@gmail.com>
2018-07-01 18:49:21 -0600

    Added initialization script. Removed experimetal code from main.

--- __main__.py
+++ __main__.py
@@ -9,19 +9,7 @@ GPIO_PB_ = 25
 GPIO_SWITCH_FULL = 21
 GPIO_SWITCH_EMPTY = 20
 
-print "Initializing stepper"
+print "Initializing Stepper"
 stepper = Stepper(GPIO_PA, GPIO_PA_, GPIO_PB, GPIO_PB_, GPIO_SWITCH_FULL, GPIO_SWITCH_EMPTY)
-
-print "MAX CCW"
-stepper.step_max_ccw()
-
-print "MAX CW"
-stepper.step_max_cw()
-
-count = 100
-while(count > 0):
-	count -= 1
-	stepper.step(10, Stepper.DIR_CCW)
-	stepper.off()
-	sleep(0.5)
-
+print "Turning off Stepper"
+stepper.off()
--- stepper.py
+++ stepper.py
@@ -83,6 +83,3 @@ class Stepper():
 
         return count
 
-
-
-
